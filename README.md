f1b4932ce3924c05c5194771e797afd803995116
Pedro Gabriel Lima <pedrogglima@gmail.com>
2019-05-18 14:08:51 -0300

    Initial setup

---
+++ README.md
@@ -0,0 +1,25 @@
+## The app
+
+In construction...
+
+## How to use
+
+```
+# On your terminal
+
+git clone git://github.com/pedrogglima/frontend-sample-react
+
+# Then install the dependencies
+
+npm install
+
+# After, inside the folder frontend-sample-react
+
+...
+
+# After starting the server, you can find the app on localhost:xxxx
+```
+
+## Licenses and Copyright
+
+Copyright (C) 2019 Pedro Gabriel Lima.  
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const path = require('path');
+const dirPublic = path.resolve(__dirname, 'public');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: dirPublic,
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: dirPublic,
+    port: 3000,
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.s[a|c]ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(jpg|png|gif|jpeg|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
+  },
+};
