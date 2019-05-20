eac47270e44bf5de2890d45615f09965c5d07867
Pedro Gabriel Lima <pedrogglima@gmail.com>
2019-05-20 14:07:47 -0300

    added style to component Users

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 //import { Route } from 'react-router-dom';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+
+import Progress from './shared/Progress';
+
 //import { client } from '../lib/Client';
 
 const USERS = [
-  '23O4F21GDWiGd33tFN3ZgI',
-  '3AQgdwMNCiN7awXch5fAaG',
-  '1kmyirVya5fRxdjsPFDM05',
-  '6ymZBbRSmzAvoSGmwAFoxm',
-  '4Mw9Gcu1LT7JaipXdwrq1Q',
+  {
+    id: '7893AQgdwMNCiN7awXch5fAaG',
+    firstName:'John',
+    lastName:'Smith',
+    avatar:'url1'
+  },
+  {
+    id: '5673AQgdwMNCiN7awXch5fAaG',
+    firstName:'Adam',
+    lastName:'Rocky',
+    avatar:'url2'
+  },
+  {
+    id: '123AQgdwMNCiN7awXch5fAaG',
+    firstName:'San',
+    lastName:'Toddy',
+    avatar:'url3'
+  },
+  {
+    id: '003AQgJAhuquAKpo4qp5fAaG',
+    firstName:'Humbert',
+    lastName:'Oliver',
+    avatar:'url4'
+  }
 ];
 
 class Users extends Component {
@@ -24,18 +53,41 @@ class Users extends Component {
     // await client.getUsers();
     this.setState({
       fetched: true,
-      albums: USERS,
+      users: USERS,
     });
   };
 
   render() {
     if (!this.state.fetched) {
       return (
-        <div>loading...</div>
+        <Progress />
       );
     } else {
       return (
-        <div>Show List of Users</div>
+        <List >
+        {this.state.users.map(user => (
+          <ListItem key={user.id} button>
+            <ListItemAvatar>
+              <Avatar
+                alt={`Avatar ${user.avatar}`}
+                src={'#'}
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary={user.lastName}
+              secondary={user.firstName}
+            />
+            <ListItemSecondaryAction>
+            <Button href="#text-buttons">
+              Edit
+            </Button>
+            <Button href="#text-buttons">
+              Delete
+            </Button>
+            </ListItemSecondaryAction>
+          </ListItem>
+        ))}
+      </List>
       );
     }
   }
