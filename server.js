d9d9b448b54c43f2c24bd66521b280a85ce5f17a
Pedro Gabriel Lima <pedrogglima@gmail.com>
2019-05-27 12:26:48 -0300

    added server Express to project

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '/public/index.html'));
+});
+
+app.listen(port, () => console.log('Ready.'));
